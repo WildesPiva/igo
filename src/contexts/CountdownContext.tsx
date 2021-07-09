eac50ea1f4ef5d2292758375cd5b06034f74e612
WildesPiva <wildeshenrique8@gmail.com>
2021-07-09 18:49:41 -0300

    fix time challenges

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -18,7 +18,7 @@ let countdownTimeout: NodeJS.Timeout
 export const CountdownContext = createContext({} as CountdownContextData)
 
 export function CountdownProvider({ children }: CountdownProviderProps) {
-  const timeToAwait = .1 // minutes
+  const timeToAwait = 30 // minutes
   const { startNewChallenge } = useContext(ChallengesContext)
   const [time, setTime] = useState(timeToAwait * 60)
   const [isActive, setIsActive] = useState(false)
